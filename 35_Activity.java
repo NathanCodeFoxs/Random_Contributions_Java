b1c68054b0867c9bbb6007b18303c57469ea6b93
Eric Nathan A. Delacruz <nathancodefoxs@gmail.com>
2024-11-19 21:05:56 +0800

    Create 35_Activity.java

---
+++ 35_Activity.java
@@ -0,0 +1,5 @@
+public int 35_Activity(int num1, int num2) { // Access Specifier: public, Method Name: multiplyNumbers, Return Type: int, Parameter List: (int num1, int num2)
+    // Method Body starts here
+    int product = num1 * num2; // This line calculates the product of num1 and num2
+    return product; // The method returns the calculated product
+} // Method ends here
